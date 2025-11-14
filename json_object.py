713b713d8d00c7140d97f993705efb6b3b13c0d0
kaspercio <147722983+kaspercio@users.noreply.github.com>
2025-11-14 13:27:46 +0000

    refactor: json object code moved into main

--- json_object.py
+++ json_object.py
@@ -1,20 +1,12 @@
-from pathlib import Path
 import json
 
 class JsonManager:
 
         def __init__(self, list):
-                self.list = list
-                # find and designate file path for json file
-                self.json_directory = Path.home() / "urls.json"     
+                self.list = list    
                 
-                # create json file if not already exists
-                if not self.json_directory.is_file():
-                        with open(self.json_directory, "w") as f:
-                                json.dump([], f, indent=4)
-                                print(f"urls.json initialised at {self.json_directory}")
-        
 
         def write_json(self, cd):
                 with open(cd, "w") as f:
                         json.dump(self.list, f, indent=4)
+
