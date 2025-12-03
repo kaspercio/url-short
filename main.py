b9c619f0d7907c87639ecddd69ac9d46f3bbf9e0
kaspercio <147722983+kaspercio@users.noreply.github.com>
2025-12-03 21:25:20 +0000

    patch allow local requests for development

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
 from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 class ShortenRequest(BaseModel):
         url: str
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (for development)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
